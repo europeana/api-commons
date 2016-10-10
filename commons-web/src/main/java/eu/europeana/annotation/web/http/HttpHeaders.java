9cfb912f7a9ba1e61ea30bfa626b8baefc7c6d37
gsergiu <sergiu.gordea@ait.ac.at>
2016-10-10 15:32:03 +0200

    #2 updated the LDP content type value to include charset
    Change-Id: I31ec1afdde411fc617a4edd4d8e6607cdfcdb75b

--- commons-web/src/main/java/eu/europeana/annotation/web/http/HttpHeaders.java
+++ commons-web/src/main/java/eu/europeana/annotation/web/http/HttpHeaders.java
@@ -26,7 +26,7 @@ public interface HttpHeaders extends javax.ws.rs.core.HttpHeaders{
 	public static final String VALUE_LDP_CONTAINER = "<http://www.w3.org/ns/ldp#Resource>; rel=\"type\"\n"+
 			"<http://www.w3.org/TR/annotation-protocol/constraints>;\n" +
 			"rel=\"http://www.w3.org/ns/ldp#constrainedBy\"";
-	public static final String VALUE_LDP_CONTENT_TYPE = "application/ld+json; profile=\"http://www.w3.org/ns/anno.jsonld\"";
+	public static final String VALUE_LDP_CONTENT_TYPE = CONTENT_TYPE_JSONLD_UTF8 + "; profile=\"http://www.w3.org/ns/anno.jsonld\"";
 	public static final String VALUE_CONSTRAINTS = "<http://www.w3.org/TR/annotation-protocol/constraints>; " +
 			"rel=\"http://www.w3.org/ns/ldp#constrainedBy\"";
 	
