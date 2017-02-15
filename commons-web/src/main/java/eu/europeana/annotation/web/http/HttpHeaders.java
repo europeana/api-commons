ea0bcf9e1d17802e27367a1da2074bbaf76e154a
shsdev <sschlarb@gmail.com>
2017-02-15 11:12:09 +0100

    removed annotation specific parameters https://github.com/europeana/annotation/issues/123
    Change-Id: I45a9fd62b3d84e3b3060e3a5461894b0d30bdc2a

--- commons-web/src/main/java/eu/europeana/annotation/web/http/HttpHeaders.java
+++ commons-web/src/main/java/eu/europeana/annotation/web/http/HttpHeaders.java
@@ -24,18 +24,7 @@ public interface HttpHeaders extends javax.ws.rs.core.HttpHeaders{
 	public static final String CONTENT_TYPE_JSONLD_UTF8 = "application/ld+json; charset=utf-8";
 	
 	
-	public static final String VALUE_LDP_RESOURCE = "<http://www.w3.org/ns/ldp#Resource>; rel=\"type\"";
-	public static final String VALUE_LDP_CONTAINER = "<http://www.w3.org/ns/ldp#Resource>; rel=\"type\"\n"+
-			"<http://www.w3.org/TR/annotation-protocol/constraints>;\n" +
-			"rel=\"http://www.w3.org/ns/ldp#constrainedBy\"";
-	public static final String VALUE_LDP_CONTENT_TYPE = CONTENT_TYPE_JSONLD_UTF8 + "; profile=\"http://www.w3.org/ns/anno.jsonld\"";
-	public static final String VALUE_CONSTRAINTS = "<http://www.w3.org/TR/annotation-protocol/constraints>; " +
-			"rel=\"http://www.w3.org/ns/ldp#constrainedBy\"";
-	
-	
-	public static final String VALUE_PREFER_CONTAINEDIRIS = "return=representation;include=\"http://www.w3.org/ns/oa#PreferContainedIRIs\"";
-	public static final String VALUE_PREFER_CONTAINEDDESCRIPTIONS = "return=representation;include=\"http://www.w3.org/ns/oa#PreferContainedDescriptions\"";
-	
+	public static final String VALUE_LDP_RESOURCE = "<http://www.w3.org/ns/ldp#Resource>; rel=\"type\"";	
 	
 }
 
