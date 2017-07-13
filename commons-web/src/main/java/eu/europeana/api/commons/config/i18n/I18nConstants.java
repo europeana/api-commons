73a0b362d42c764ff840ae438560169016abb930
gsergiu <sergiu.gordea@ait.ac.at>
2017-07-13 14:11:08 +0200

    #2 added generic exceptions and internationalization constants
    Change-Id: I19506988a48b524975d526787b2540b8dbde48e0

---
+++ commons-web/src/main/java/eu/europeana/api/commons/config/i18n/I18nConstants.java
@@ -0,0 +1,13 @@
+package eu.europeana.api.commons.config.i18n;
+
+public interface I18nConstants {
+	
+	//401
+	static final String INVALID_APIKEY = "error.invalid_apikey";
+	static final String EMPTY_APIKEY = "error.empty_apikey";
+	
+	//500
+	static final String SERVER_ERROR_UNEXPECTED = "error.server_unexpected_error";
+	
+	
+}
---
+++ commons-web/src/main/java/eu/europeana/api/commons/web/exception/ApplicationAuthenticationException.java
@@ -0,0 +1,26 @@
+package eu.europeana.api.commons.web.exception;
+
+
+import org.springframework.http.HttpStatus;
+
+public class ApplicationAuthenticationException extends HttpException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8994054535719881829L;
+	
+	public ApplicationAuthenticationException(String message, String i18nKey){
+		super(message, i18nKey, null, HttpStatus.UNAUTHORIZED);
+	}
+
+	public ApplicationAuthenticationException(String message, String i18nKey, String[] i18nParams, HttpStatus status, Throwable th){
+		super(message, i18nKey, i18nParams, status, th);
+		
+	}
+	
+	public ApplicationAuthenticationException(String message, String i18nKey, String[] i18nParams) {
+		super(message, i18nKey, i18nParams, HttpStatus.UNAUTHORIZED);
+	}
+
+}
