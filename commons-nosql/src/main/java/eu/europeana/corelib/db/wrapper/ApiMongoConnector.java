cebaa7fe4d9ecb3fcc975cb6e85d78f78e2d2c00
gsergiu <sergiu.gordea@ait.ac.at>
2017-11-19 16:30:30 +0100

    #14 reverted back mongo driver, added support for SSL Context initialization
    Change-Id: I1a3334659de7f53f1f1855ba69c8400c5349ffde

--- commons-nosql/src/main/java/eu/europeana/corelib/db/wrapper/ApiMongoConnector.java
+++ commons-nosql/src/main/java/eu/europeana/corelib/db/wrapper/ApiMongoConnector.java
@@ -16,11 +16,29 @@
  */
 package eu.europeana.corelib.db.wrapper;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URL;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.cert.CertificateException;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManagerFactory;
+
+import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
 import org.mongodb.morphia.Datastore;
 import org.mongodb.morphia.Morphia;
 
 import com.mongodb.MongoClient;
+import com.mongodb.MongoClientException;
+import com.mongodb.MongoClientOptions;
 import com.mongodb.MongoClientURI;
 import com.mongodb.MongoException;
 
@@ -31,6 +49,7 @@ import com.mongodb.MongoException;
 public class ApiMongoConnector {
 
 	private static Logger log = Logger.getLogger(ApiMongoConnector.class);
+	private SSLContext sslContext = null;
 
 	/**
 	 * Default constructor
@@ -38,24 +57,88 @@ public class ApiMongoConnector {
 	public ApiMongoConnector() {
 	}
 
+
 	/**
 	 * Mongo create datastore
-	 * @param connectionUri - the connection URI, without database name 
-	 * @param dbName - database name
+	 * 
+	 * @param connectionUri
+	 *            - the connection URI, without database name
+	 * @param trustore 
+	 *            - the name of the trustore file available in classpath in the /config folder 
 	 * @return the datastore
 	 */
-	public Datastore createDatastore(String connectionUri, String dbName) {
+	public Datastore createDatastore(String connectionUri, String truststore, String truststorePass) {
 		Datastore datastore = null;
 		Morphia connection = new Morphia();
 		try {
-			log.info(String.format("Connecting to '%s' mongo server: %s", dbName, connectionUri));
-			MongoClient mongoClient = new MongoClient(new MongoClientURI(connectionUri));
-				datastore = connection.createDatastore(mongoClient, dbName);
-			log.info(String.format(
-					"Connection to db '%s' mongo server was successful", dbName));
+			log.debug("Connecting to mongo server:" + connectionUri);
+			
+			MongoClientOptions.Builder mco = MongoClientOptions.builder();
+			if(isSslEnabled(connectionUri)){
+				validateTrustStoreConfig(truststore, truststorePass);
+				log.debug("Enabling ssl connection using truststore: " + truststore + ":"+truststorePass);
+				SSLContext sc = getSslContext(truststore, truststorePass);
+				mco.sslEnabled(true).socketFactory(sc.getSocketFactory());
+			}
+			
+			MongoClientURI mongoUri = new MongoClientURI(connectionUri, mco);
+			MongoClient mongoClient = new MongoClient(mongoUri);
+			datastore = connection.createDatastore(mongoClient, mongoUri.getDatabase());
+
+			log.info(String.format("Connection to db '%s' mongo server was successful", mongoUri.getDatabase()));
 		} catch (MongoException e) {
-			log.error(e.getMessage());
+			//redundant, runtime exceptions should be logged by the system 
+			//log.error(e.getMessage());
+			throw e;
 		}
 		return datastore;
+	}
+
+	protected void validateTrustStoreConfig(String truststore, String truststorePass) {
+		//store and pass are mandatory
+		if(StringUtils.isEmpty(truststore) || StringUtils.isEmpty(truststorePass))
+			throw new IllegalArgumentException("Trustore and truststorePass must not be null when ssl is enabled! " 
+					+ truststore + ":"+truststorePass);
+	}
+
+	private boolean isSslEnabled(String connectionUri) {
+		return connectionUri.contains("ssl=true");
+	}
+
+	private SSLContext initSSLContext(String truststore, String truststorePass) {
+		try {
+			// TODO - make keystore type and SSL version configurable
+			String trustStoreLocation = "/config/" + truststore;
+			URL trustStoreUri = getClass().getResource(trustStoreLocation);
+			if (trustStoreUri == null)
+				throw new FileNotFoundException("cannot find trustore file in classpath: " + trustStoreLocation);
+
+			KeyStore jks = KeyStore.getInstance("JKS");
+
+			jks.load(new FileInputStream(new File(trustStoreUri.getFile())), truststorePass.toCharArray());
+
+			String tmfAlgorithm = TrustManagerFactory.getDefaultAlgorithm();
+			TrustManagerFactory tmf = TrustManagerFactory.getInstance(tmfAlgorithm);
+			tmf.init(jks);
+
+			sslContext = SSLContext.getInstance("TLSv1.2");
+			sslContext.init(null, tmf.getTrustManagers(), new SecureRandom());
+			return sslContext;
+		} catch (KeyManagementException | IOException | KeyStoreException | NoSuchAlgorithmException
+				| CertificateException ex) {
+
+			throw new MongoClientException("Cannot initialize mongo truststore", ex);
+		}
+	}
+
+	public SSLContext getSslContext(String truststore, String truststorePass) {
+		// lazy initialization
+		if (sslContext == null)
+			initSSLContext(truststore, truststorePass);
+		return sslContext;
+	}
+
+	void setSslContext(SSLContext sslContext) {
+		this.sslContext = sslContext;
 	}
 }
