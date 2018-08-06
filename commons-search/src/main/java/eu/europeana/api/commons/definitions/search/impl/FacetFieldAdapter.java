dbc841b5aef8067147222cddd135ee880736599f
gsergiu <sergiu.gordea@ait.ac.at>
2018-08-06 16:22:11 +0200

    #17 added support for facet serialization
    Change-Id: Ifa5ce77713c176282cdcd16fbff991517748c486

---
+++ commons-search/src/main/java/eu/europeana/api/commons/definitions/search/impl/FacetFieldAdapter.java
@@ -0,0 +1,47 @@
+package eu.europeana.api.commons.definitions.search.impl;
+
+import java.util.Map;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+import org.apache.solr.client.solrj.response.FacetField;
+import org.apache.solr.client.solrj.response.FacetField.Count;
+
+import eu.europeana.api.commons.definitions.search.FacetFieldView;
+
+public class FacetFieldAdapter implements FacetFieldView {
+
+	private FacetField solrFacetField; 
+	private SortedMap<String, Long> valueCountMap;
+	
+	
+	public FacetFieldAdapter(FacetField solrFacetField) {
+		this.solrFacetField = solrFacetField;
+	}
+
+	@Override
+	public Map<String, Long> getValueCountMap() {
+		if(solrFacetField.getValues() == null || solrFacetField.getValues().isEmpty())
+			return null;
+		
+		if(valueCountMap == null){
+			valueCountMap = new TreeMap<String, Long>();
+		 
+			for(Count entry: solrFacetField.getValues()){
+				//DO not serialize empty values 
+				if(entry.getCount() > 0)
+					valueCountMap.put(entry.getName(), entry.getCount());
+			}
+			
+		}	
+		
+		return valueCountMap;
+	}
+	
+
+	@Override
+	public String getName() {
+		return solrFacetField.getName();
+	}
+
+}
