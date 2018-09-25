ce05ecf4f34a8c382c670153ebb3126dd3a1b660
rgraf <roman.graf@ait.ac.at>
2018-09-25 13:56:16 +0200

    #83 EA-1313 Updated from RESULT_PAGE to RESULT_LIST

--- commons-definitions/src/main/java/eu/europeana/api/commons/definitions/vocabulary/CommonLdConstants.java
+++ commons-definitions/src/main/java/eu/europeana/api/commons/definitions/vocabulary/CommonLdConstants.java
@@ -26,6 +26,6 @@ public interface CommonLdConstants {
 
 	public static final String COLLECTION = "Collection";
 	
-	public static final String RESULT_PAGE = "ResultPage";
+	public static final String RESULT_LIST = "ResultList";
 
 }
