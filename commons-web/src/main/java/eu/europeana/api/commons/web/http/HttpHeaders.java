f20dbade3af42d5117f843d150ae68dd4959d6e3
DenisKatic <denis.katic@ait.ac.at>
2019-05-08 16:08:11 +0200

    #EA-1643 added jsonld content type without utf8 information

--- commons-web/src/main/java/eu/europeana/api/commons/web/http/HttpHeaders.java
+++ commons-web/src/main/java/eu/europeana/api/commons/web/http/HttpHeaders.java
@@ -26,6 +26,7 @@ public interface HttpHeaders extends javax.ws.rs.core.HttpHeaders{
 	
 	public static final String CONTENT_TYPE_JSON_UTF8 = MediaType.APPLICATION_JSON_VALUE+";charset=utf-8";
 	public static final String CONTENT_TYPE_JSONLD_UTF8 = "application/ld+json;charset=utf-8";
+	public static final String CONTENT_TYPE_JSONLD = "application/ld+json";
 	public static final String CONTENT_TYPE_APPLICATION_RDF_XML = "application/rdf+xml";
 	public static final String CONTENT_TYPE_RDF_XML = "rdf/xml";
 	
