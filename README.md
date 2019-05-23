017990921bab27f79bb9dcc8b3e700ec276af606
Luthien-in-edhil <maike.dulk@europeana.eu>
2019-05-23 16:32:03 +0200

    EA-1664+1665_updated-license

--- README.md
+++ README.md
@@ -3,6 +3,6 @@ Shared code/modules for API development.
 
 ## License
 
-Licensed under the EUPL V.1.1.
+Licensed under the EUPL V.1.2
 
 For full details, see [LICENSE.md](LICENSE.md).
--- commons-oauth/src/main/java/eu/europeana/api/commons/oauth2/service/impl/BridgeApiTokenStoreService.java
+++ commons-oauth/src/main/java/eu/europeana/api/commons/oauth2/service/impl/BridgeApiTokenStoreService.java
@@ -1,24 +1,4 @@
-package eu.europeana.api.commons.oauth2.service.impl;
-/*
- * Copyright 2007-2015 The Europeana Foundation
- *
- * Licenced under the EUPL, Version 1.1 (the "Licence") and subsequent versions as approved
- * by the European Commission;
- * You may not use this work except in compliance with the Licence.
- *
- * You may obtain a copy of the Licence at:
- * http://joinup.ec.europa.eu/software/page/eupl
- *
- * Unless required by applicable law or agreed to in writing, software distributed under
- * the Licence is distributed on an "AS IS" basis, without warranties or conditions of
- * any kind, either express or implied.
- * See the Licence for the specific language governing permissions and limitations under
- * the Licence.
- */
-
-
-
-import java.util.ArrayList;
+package eu.europeana.api.commons.oauth2.service.impl;import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
