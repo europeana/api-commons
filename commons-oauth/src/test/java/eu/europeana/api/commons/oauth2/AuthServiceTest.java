966fe93cca42f5152de0cf694f816b7526858a40
gsergiu <sergiu.gordea@ait.ac.at>
2019-07-23 09:38:25 +0200

    #36 ignore integration test while building
    Change-Id: Id04ae2459cb585689bab52a68dd3740d49a6d6d2

--- commons-oauth/src/test/java/eu/europeana/api/commons/oauth2/AuthServiceTest.java
+++ commons-oauth/src/test/java/eu/europeana/api/commons/oauth2/AuthServiceTest.java
@@ -5,10 +5,10 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
-import org.apache.http.HttpHeaders;
 import org.apache.http.HttpStatus;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import eu.europeana.apikey.client.ApiKeyValidationResult;
@@ -21,18 +21,19 @@ import eu.europeana.apikey.client.exception.ApiKeyValidationException;
  * @author GrafR
  *
  */
+@Ignore("Integration test - manually activate for local testing") 
 public class AuthServiceTest {
 
 	private final Logger log = LogManager.getLogger(getClass());
 	
-	private String TEST_API_KEY = "tromssusequ";
 	private String WRONG_API_KEY = "wrongapikey";
 
 	@Test
 	public void testAuthValidationReadAccess() throws ApiKeyValidationException {
 
     	    Client client = new Client();
-    	    ApiKeyValidationResult validationResult = client.validateApiKey(
+    	    String TEST_API_KEY = "CHANGE TO VALID API KEY";
+	    ApiKeyValidationResult validationResult = client.validateApiKey(
     		TEST_API_KEY);
     	    assertNotNull(validationResult);
     	    assertTrue(validationResult.isValidApiKey());
