417b059347a370f530ec8c6eebb044fd96d78ad0
srishtisingh-eu <srishti.singh@europeana.eu>
2021-03-31 13:22:43 +0200

    # added index for set-api testing

--- commons-nosql/src/main/java/eu/europeana/corelib/db/wrapper/ApiMongoConnector.java
+++ commons-nosql/src/main/java/eu/europeana/corelib/db/wrapper/ApiMongoConnector.java
@@ -76,7 +76,7 @@ public class ApiMongoConnector {
 			mongoClient = new MongoClient(mongoUri);
 			
 			datastore = connection.createDatastore(mongoClient, mongoUri.getDatabase());
-			//datastore.ensureIndexes();
+			datastore.ensureIndexes();
 			log.info(String.format("Connection to db '%s' mongo server was successful", mongoUri.getDatabase()));
 		} catch (MongoException e) {
 			//runtime exceptions will be logged by the system 
