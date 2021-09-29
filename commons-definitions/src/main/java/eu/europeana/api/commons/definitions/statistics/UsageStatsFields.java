7c78034f971288e98808b2fa6a6e4ba002147e6e
srishti <srishti.singh@europeana.eu>
2021-09-29 14:03:23 +0200

    EA-2694 entity per type view added

--- commons-definitions/src/main/java/eu/europeana/api/commons/definitions/statistics/UsageStatsFields.java
+++ commons-definitions/src/main/java/eu/europeana/api/commons/definitions/statistics/UsageStatsFields.java
@@ -12,7 +12,8 @@ public class UsageStatsFields {
     public static final String SETS_PER_USER      = "AverageSetsPerUser";
 
     // entity constants
-    public static final String ENTITIES_PER_LANG_TYPE  = "entities";
+    public static final String ENTITIES_PER_LANG = "entitiesPerLanguage";
+    public static final String ENTITIES_PER_TYPE = "entitiesPerType";
     public static final String ENTITY_LANG   = "language";
     public static final String TIMESPAN      = "timespan";
     public static final String PLACE         = "place";
