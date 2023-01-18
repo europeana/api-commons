a576a1ae609065cc9538c306174c3173e1384410
StevaneticS <StevaneticS@N3DSS2228.D03.arc.local>
2023-01-18 12:12:13 +0100

    added language utils class

---
+++ commons-definitions/src/main/java/eu/europeana/api/commons/definitions/utils/LanguageUtils.java
@@ -0,0 +1,10 @@
+package eu.europeana.api.commons.definitions.utils;
+
+import java.util.Locale;
+import java.util.Set;
+
+public class LanguageUtils {
+
+  public static final Set<String> ISO_LANGUAGES = Set.of(Locale.getISOLanguages());
+
+}
