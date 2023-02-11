708a920e7a71975c57f3ce6ccef3c4604423316a
GordeaS <4517853+gsergiu@users.noreply.github.com>
2023-02-11 12:22:47 +0100

    added support to read keystore with absolute path configuration #EA-3283

--- commons-nosql/src/main/java/eu/europeana/corelib/db/wrapper/ApiMongoConnector.java
+++ commons-nosql/src/main/java/eu/europeana/corelib/db/wrapper/ApiMongoConnector.java
@@ -1,5 +1,7 @@
 package eu.europeana.corelib.db.wrapper;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
@@ -29,163 +31,197 @@ import com.mongodb.MongoException;
  *
  */
 public class ApiMongoConnector {
-	private static final Logger log = LogManager.getLogger(ApiMongoConnector.class);
-	SSLContext sslContext = null;
-	MongoClient mongoClient = null;
-	int DEFAULT_IDLE_TIME = 5000;  
-	
-	/**
-	 * Default constructor
-	 */
-	public ApiMongoConnector() {
-	}
+  private static final Logger log = LogManager.getLogger(ApiMongoConnector.class);
+  SSLContext sslContext = null;
+  MongoClient mongoClient = null;
+  int DEFAULT_IDLE_TIME = 5000;
+
+  /**
+   * Default constructor
+   */
+  public ApiMongoConnector() {}
 
 
-	/**
-	 * Mongo create datastore
-	 * 
-	 * @param connectionUri
-	 *            - the connection URI, without database name
-	 * @param truststore 
-	 *            - the name of the trustore file available in classpath in the /config folder 
-	 * @param truststorePass 
-	 *            - the password needd to access the  trustore  
-	 *            
-	 * @return the datastore
-	 */
-	public Datastore createDatastore(String connectionUri, String truststore, String truststorePass) {
-	    return createDatastore(connectionUri, truststore, truststorePass, DEFAULT_IDLE_TIME);
-	}
+  /**
+   * Mongo create datastore
+   * 
+   * @param connectionUri - the connection URI, without database name
+   * @param truststore - the name of the trustore file available in classpath in the /config folder
+   * @param truststorePass - the password needd to access the trustore
+   * 
+   * @return the datastore
+   */
+  public Datastore createDatastore(String connectionUri, String truststore, String truststorePass) {
+    return createDatastore(connectionUri, truststore, truststorePass, DEFAULT_IDLE_TIME);
+  }
 
-	public Datastore createDatastore(String connectionUri, String truststore, String truststorePass, int maxConnectionIdleTime) {
-	  return createDatastore(connectionUri, truststore, truststorePass, maxConnectionIdleTime, null); 
-	}
-	
-	public Datastore createDatastore(String connectionUri, String truststore, String truststorePass, int maxConnectionIdleTime, String mongoModelPackage) {
-		Datastore datastore = null;
-		Morphia connection;
-		try {
-			log.debug("Connecting to mongo server:" + connectionUri);
-			
-			MongoClientOptions.Builder mco = buildMongoConnectionOptions(connectionUri, truststore, truststorePass);
-			if(maxConnectionIdleTime > 0) {
-			    mco.maxConnectionIdleTime(maxConnectionIdleTime);
-			} else {
-			    mco.maxConnectionIdleTime(DEFAULT_IDLE_TIME);
-			} 
-			
-			MongoClientURI mongoUri = new MongoClientURI(connectionUri, mco);
-			mongoClient = new MongoClient(mongoUri);
-			
-			if (StringUtils.isNotBlank(mongoModelPackage)) {
-              
-              MapperOptions options = new MapperOptions();
-              options.setMapSubPackages(true);
-              Mapper packageMapper = new Mapper(options);
-              connection = new Morphia(packageMapper);
-              //map classes
-              connection.mapPackage(mongoModelPackage, false);
-              datastore = connection.createDatastore(mongoClient, mongoUri.getDatabase());
-              
-              //to create indexes
-              datastore.ensureIndexes();
-              
-            } else {
-              connection = new Morphia();
-              datastore = connection.createDatastore(mongoClient, mongoUri.getDatabase());  
-            }
-			log.info(String.format("Connection to db '%s' mongo server was successful", mongoUri.getDatabase()));
-		} catch (MongoException e) {
-			//runtime exceptions will be logged by the system 
-			throw e;
-		}
-		return datastore;
-	}
+  public Datastore createDatastore(String connectionUri, String truststore, String truststorePass,
+      int maxConnectionIdleTime) {
+    return createDatastore(connectionUri, truststore, truststorePass, maxConnectionIdleTime, null);
+  }
 
-	private MongoClientOptions.Builder buildMongoConnectionOptions(String connectionUri, String truststore,
-			String truststorePass) {
-		
-		MongoClientOptions.Builder mco = MongoClientOptions.builder();
-		
-		if(isSslEnabled(connectionUri)){
-			//allow invalid host names (required for host aliases)
-			mco.sslEnabled(true).sslInvalidHostNameAllowed(true);
-			
-			//if the trustore is configured will be used for mongo connection
-			if(!StringUtils.isEmpty(truststore)){
-				validateTrustStoreConfig(truststore, truststorePass);
-				log.debug("Enabling ssl connection using truststore: " + truststore + ":"+truststorePass);
-				SSLContext sc = getSslContext(truststore, truststorePass);
-				mco.socketFactory(sc.getSocketFactory());	
-			}		
-		}
-		return mco;
-	}
+  public Datastore createDatastore(String connectionUri, String truststore, String truststorePass,
+      int maxConnectionIdleTime, String mongoModelPackage) {
+    Datastore datastore = null;
+    Morphia connection;
+    try {
+      log.debug("Connecting to mongo server:" + connectionUri);
 
-	 /**
-     * Close the connection to mongo
-     */
-    public void close() {
-        if(mongoClient != null){
-        	try{
-		    	log.info("Shutting down connections to Mongo...");
-		        mongoClient.close();
-        	}catch(Throwable th){
-        		log.error("cannot close mongo connetions", th);
-        	}
-        }
+      MongoClientOptions.Builder mco =
+          buildMongoConnectionOptions(connectionUri, truststore, truststorePass);
+      if (maxConnectionIdleTime > 0) {
+        mco.maxConnectionIdleTime(maxConnectionIdleTime);
+      } else {
+        mco.maxConnectionIdleTime(DEFAULT_IDLE_TIME);
+      }
+
+      MongoClientURI mongoUri = new MongoClientURI(connectionUri, mco);
+      mongoClient = new MongoClient(mongoUri);
+
+      if (StringUtils.isNotBlank(mongoModelPackage)) {
+
+        MapperOptions options = new MapperOptions();
+        options.setMapSubPackages(true);
+        Mapper packageMapper = new Mapper(options);
+        connection = new Morphia(packageMapper);
+        // map classes
+        connection.mapPackage(mongoModelPackage, false);
+        datastore = connection.createDatastore(mongoClient, mongoUri.getDatabase());
+
+        // to create indexes
+        datastore.ensureIndexes();
+
+      } else {
+        connection = new Morphia();
+        datastore = connection.createDatastore(mongoClient, mongoUri.getDatabase());
+      }
+      log.info(String.format("Connection to db '%s' mongo server was successful",
+          mongoUri.getDatabase()));
+    } catch (MongoException e) {
+      // runtime exceptions will be logged by the system
+      throw e;
+    }
+    return datastore;
+  }
+
+  private MongoClientOptions.Builder buildMongoConnectionOptions(String connectionUri,
+      String truststore, String truststorePass) {
+
+    MongoClientOptions.Builder mco = MongoClientOptions.builder();
+
+    if (isSslEnabled(connectionUri)) {
+      // allow invalid host names (required for host aliases)
+      mco.sslEnabled(true).sslInvalidHostNameAllowed(true);
+
+      // if the trustore is configured will be used for mongo connection
+      if (!StringUtils.isEmpty(truststore)) {
+        validateTrustStoreConfig(truststore, truststorePass);
+        log.debug("Enabling ssl connection using truststore: " + truststore + ":" + truststorePass);
+        SSLContext sc = getSslContext(truststore, truststorePass);
+        mco.socketFactory(sc.getSocketFactory());
+      }
+    }
+    return mco;
+  }
+
+  public SSLContext getSslContext(String truststore, String truststorePass) {
+    if (sslContext != null){
+      return sslContext;  
     }
     
-	protected void validateTrustStoreConfig(String truststore, String truststorePass) {
-		//store and pass are mandatory
-		if(StringUtils.isEmpty(truststore) || StringUtils.isEmpty(truststorePass))
-			throw new IllegalArgumentException("Both trustore and truststorePass must be provided, when trustore is used! " 
-					+ truststore + ":"+truststorePass);
-	}
+    // lazy initialization
+    if (truststore.startsWith("/")) {
+        return loadTrustStoreFromFile(truststore, truststorePass);
+    } else {
+        return loadTrustStoreFromClasspath(truststore, truststorePass); 
+    }
+  }
+  
+  /**
+   * Close the connection to mongo
+   */
+  public void close() {
+    if (mongoClient != null) {
+      try {
+        log.info("Shutting down connections to Mongo...");
+        mongoClient.close();
+      } catch (Throwable th) {
+        log.error("cannot close mongo connetions", th);
+      }
+    }
+  }
 
-	private boolean isSslEnabled(String connectionUri) {
-		return connectionUri.contains("tls=true") || connectionUri.contains("ssl=true");
-	}
+  protected void validateTrustStoreConfig(String truststore, String truststorePass) {
+    // store and pass are mandatory
+    if (StringUtils.isEmpty(truststore) || StringUtils.isEmpty(truststorePass))
+      throw new IllegalArgumentException(
+          "Both trustore and truststorePass must be provided, when trustore is used! " + truststore
+              + ":" + truststorePass);
+  }
 
-	private SSLContext initSSLContext(String truststore, String truststorePass) {
-		
-		// TODO - make keystore type and SSL version configurable
-		String trustStoreLocation = "/" + truststore;
-		URL trustStoreUri = getClass().getResource(trustStoreLocation);
-		if (trustStoreUri == null) {
-		  //fallback for backward compatibility
-		  log.info("truststore not at location: {}, search in config subfolder.", trustStoreLocation);
-          //search in config subfolder
-		  trustStoreLocation = "/config/" + truststore;
-		  trustStoreUri = getClass().getResource(trustStoreLocation);
-		  if (trustStoreUri == null) {
-		    throw new MongoClientException("cannot find trustore file in classpath: " + trustStoreLocation);
-		  }
-		}
-		
-		try(InputStream stream = getClass().getResourceAsStream(trustStoreLocation)) {
-			KeyStore jks = KeyStore.getInstance("JKS");
-			jks.load(stream, truststorePass.toCharArray());
+  private boolean isSslEnabled(String connectionUri) {
+    return connectionUri.contains("tls=true") || connectionUri.contains("ssl=true");
+  }
 
-			String tmfAlgorithm = TrustManagerFactory.getDefaultAlgorithm();
-			TrustManagerFactory tmf = TrustManagerFactory.getInstance(tmfAlgorithm);
-			tmf.init(jks);
+  private SSLContext loadTrustStoreFromClasspath(String truststore, String truststorePass) {
+    log.debug("Loading trustore from classpath: {}", truststore);
+    String trustStoreLocation = "/" + truststore;
+    URL trustStoreUri = getClass().getResource(trustStoreLocation);
+    if (trustStoreUri == null) {
+      // fallback for backward compatibility, search in /config/folder
+      log.info("truststore not at location: {}, search in config subfolder.", trustStoreLocation);
+      trustStoreLocation = "/config/" + truststore;
+      trustStoreUri = getClass().getResource(trustStoreLocation);
+      if (trustStoreUri == null) {
+        log.info("truststore not at location: {}, search in config subfolder.", trustStoreLocation);
+        throw new MongoClientException(
+            "cannot find trustore file in classpath: " + trustStoreLocation);
+      }
+    }
+    
+    try (InputStream stream = getClass().getResourceAsStream(trustStoreLocation)) {
+      return loadCertificateToSslContext(stream, truststorePass);
+    } catch (KeyManagementException | IOException | KeyStoreException | NoSuchAlgorithmException
+        | CertificateException ex) {
+      throw new MongoClientException("Cannot initialize mongo truststore", ex);
+    }
+  }
 
-			//add the TLS protocol version to configurations when needed 
-			sslContext = SSLContext.getInstance("TLSv1.2");
-			sslContext.init(null, tmf.getTrustManagers(), new SecureRandom());
-			return sslContext;
-		} catch (KeyManagementException | IOException | KeyStoreException | NoSuchAlgorithmException
-				| CertificateException ex) {
-			throw new MongoClientException("Cannot initialize mongo truststore", ex);
-		}
-	}
 
-	public SSLContext getSslContext(String truststore, String truststorePass) {
-		// lazy initialization
-		if (sslContext == null)
-			initSSLContext(truststore, truststorePass);
-		return sslContext;
-	}
-	
+  private SSLContext loadTrustStoreFromFile(String truststore, String truststorePass) {
+    //if absolute file path read from file system
+    File trustoreFile = new File(truststore); 
+    if (!trustoreFile.exists()) {
+      throw new MongoClientException("Trustore file location does not exist: " + truststore);
+    }
+      // fallback for backward compatibility
+      log.debug("Loading truststore from file {}", trustoreFile);
+      
+      try (InputStream stream = new FileInputStream(trustoreFile)) {
+        return loadCertificateToSslContext(stream, truststorePass);
+      } catch (KeyManagementException | IOException | KeyStoreException | NoSuchAlgorithmException
+          | CertificateException ex) {
+        throw new MongoClientException("Cannot initialize mongo truststore", ex);
+      }
+  }
+
+
+  private SSLContext loadCertificateToSslContext(InputStream stream, String truststorePass)
+      throws KeyStoreException, IOException, NoSuchAlgorithmException, CertificateException,
+      KeyManagementException {
+    KeyStore jks = KeyStore.getInstance("JKS");
+    jks.load(stream, truststorePass.toCharArray());
+
+    String tmfAlgorithm = TrustManagerFactory.getDefaultAlgorithm();
+    TrustManagerFactory tmf = TrustManagerFactory.getInstance(tmfAlgorithm);
+    tmf.init(jks);
+
+    // add the TLS protocol version to configurations when needed
+    sslContext = SSLContext.getInstance("TLSv1.2");
+    sslContext.init(null, tmf.getTrustManagers(), new SecureRandom());
+    return sslContext;
+  }
+
+
+
 }
