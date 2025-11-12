5639e82ebc13ef61bac9d3e396cd8450b1950d80
srishti <srishti.singh@europeana.eu>
2025-11-12 14:54:42 +0100

    # add another constsructor

--- commons-clientutils/src/main/java/eu/europeana/api/commons/auth/AuthenticationConfig.java
+++ commons-clientutils/src/main/java/eu/europeana/api/commons/auth/AuthenticationConfig.java
@@ -1,5 +1,6 @@
 package eu.europeana.api.commons.auth;
 
+import java.util.Map;
 import java.util.Properties;
 
 import eu.europeana.api.commons.auth.service.AuthGrant;
@@ -16,7 +17,7 @@ public class AuthenticationConfig extends Properties {
     public static final String CONFIG_TOKEN_ENDPOINT = "token_endpoint";
     public static final String CONFIG_GRANT_PARAMS   = "grant_params";
     public static final String CONFIG_APIKEY         = "apikey";
-    
+
 
     public AuthenticationConfig() {
         super();
@@ -31,12 +32,23 @@ public class AuthenticationConfig extends Properties {
         putAll(properties);
     }
 
+    /**
+     * CConstructor to inject properties
+     * @param tokenEndpoint endpoint
+     * @param grantParams params
+     */
+    public AuthenticationConfig(String tokenEndpoint, String grantParams) {
+        super();
+        putAll(buildAuthConfigProps(tokenEndpoint, grantParams));
+    }
+
+
     public String getAuthTokenEndpoitUri() {
         return getProperty(CONFIG_TOKEN_ENDPOINT);
     }
 
     public String getApiKey() {
-        return containsKey(CONFIG_APIKEY) ? getProperty(CONFIG_APIKEY) 
+        return containsKey(CONFIG_APIKEY) ? getProperty(CONFIG_APIKEY)
                                           : getProperty(client_id);
     }
 
@@ -50,4 +62,18 @@ public class AuthenticationConfig extends Properties {
         }
         return new AuthGrant(this);
     }
+
+    /**
+     * method to build the authentication configuration properties
+     * @param tokenEndpoint endpoint
+     * @param grantParams params
+     * @return properties
+     */
+    private Properties buildAuthConfigProps(String tokenEndpoint, String grantParams) {
+        Properties properties = new Properties();
+        properties.setProperty(CONFIG_TOKEN_ENDPOINT, tokenEndpoint);
+        properties.setProperty(CONFIG_GRANT_PARAMS, grantParams);
+        return properties;
+    }
+
 }
