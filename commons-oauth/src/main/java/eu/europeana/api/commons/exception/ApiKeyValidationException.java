e803f1a2d028f95b2903b57e4eec97a613368a96
Shwetambara Nazare <nshweta90@gmail.com>
2025-11-12 10:10:57 +0100

    Remove dependency for apikey client jar

--- commons-oauth/src/main/java/eu/europeana/api/commons/exception/ApiKeyValidationException.java
+++ commons-oauth/src/main/java/eu/europeana/api/commons/exception/ApiKeyValidationException.java
@@ -2,8 +2,6 @@ package eu.europeana.api.commons.exception;
 
 public class ApiKeyValidationException extends Exception {
 
-  private static final long serialVersionUID = 6387090087297106543L;
-
   public ApiKeyValidationException(String errorMessage) {
     super(errorMessage);
   }
