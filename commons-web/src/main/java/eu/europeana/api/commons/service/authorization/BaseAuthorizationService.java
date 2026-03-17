bc68280aa7631530c3a972624e347c8b8bd70ca5
SristiSingh-eu <srishti.singh@europeana.eu>
2026-03-17 14:20:30 +0100

    remove unused imports

--- commons-web/src/main/java/eu/europeana/api/commons/service/authorization/BaseAuthorizationService.java
+++ commons-web/src/main/java/eu/europeana/api/commons/service/authorization/BaseAuthorizationService.java
@@ -7,9 +7,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import javax.servlet.http.HttpServletRequest;
-
-import eu.europeana.api.commons.oauth2.model.KeyValidationError;
-import eu.europeana.api.commons.oauth2.model.KeyValidationResult;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
